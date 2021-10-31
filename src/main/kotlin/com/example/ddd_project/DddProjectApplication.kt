160edfb4a2f3bbebf5fecfd440b6cffbd80eaa2d
Andreas Aasen <andreas.aasen@evimeria.se>
2021-10-31 20:52:38 +0100

    first commit with kotlin spring framework

---
+++ src/main/kotlin/com/example/ddd_project/DddProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ddd_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DddProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<DddProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ddd_project/DddProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ddd_project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DddProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
